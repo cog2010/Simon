61941d93dba45bef428ba16c9e250ece1db04aa8
Travis Blanton <Travis.blanton@ymail.com>
2017-04-24 22:39:20 -0500

    Simon
    Simon Game created for Free Code Camp. Responsive design needs work,
    but all of the javascript works.

---
+++ script.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+
+  $("#reset").hide();
+
+   var sequence = [];
+   var player = [];
+   var moveCount = 1;
+   var round = 0;
+   var active = false;
+   var strict = false;
+   var k = 1;
+   var tone1 = $("#greenSound")[0];
+   var tone2 = $("#redSound")[0];
+   var tone3 = $("#yellowSound")[0];
+   var tone4 = $("#blueSound")[0];
+
+   $("#start").on("click", function(){
+      getMoves();
+      startRound();
+      active = true;
+      $("#start").hide();
+      $("#reset").show();
+   });
+
+   function getMoves(){
+     sequence = [];
+     round = 0;
+     while (sequence.length < 20) {
+         var temp = Math.ceil(Math.random() * 4);
+         sequence.push(temp);
+      }
+      console.log(sequence);
+   }
+   function startRound() {
+      round++;
+      moveCount = 1;
+      $("#score").html("<h1>" + round + "</h1>");
+      playRound();
+      //console.log("new round");
+   }
+   function playRound(){
+      setTimeout(function(){
+       if (sequence[k-1] ===1){
+         $("#green").toggleClass("brighten");
+        tone1.play();
+         setTimeout(function(){$("#green").toggleClass("brighten");}, 250);
+      } else if (sequence[k-1] ===2){
+         $("#red").toggleClass("brighten");
+         tone2.play();
+         setTimeout(function(){$("#red").toggleClass("brighten");}, 250);
+      } else if (sequence[k-1] ===3){
+           $("#yellow").toggleClass("brighten");
+           tone3.play();
+           setTimeout(function(){$("#yellow").toggleClass("brighten");}, 250);
+       } else if (sequence[k-1] ===4){
+           $("#blue").toggleClass("brighten");
+          tone4.play();
+           setTimeout(function(){$("#blue").toggleClass("brighten");}, 250);
+    }
+    if (k < round){
+        k++
+        playRound();
+    }else{
+        k=0;
+    }
+}, 700);
+   };
+
+   function checkMove(move){
+      if (active === true){
+          if (move === sequence[moveCount-1]){
+            if (moveCount == round){
+                startRound();
+              }else{
+                moveCount ++;
+              }
+            }else if (strict == true){
+              getMoves();
+              startRound();
+              $("#score").html("<h1>!!</h1>");
+              setTimeout(function(){$("#score").html("<h1>" + round + "</h1>");},1000);
+            }else{
+              moveCount = 1;
+              playRound();
+              $("#score").html("<h1>!!</h1>");
+              setTimeout(function(){$("#score").html("<h1>" + round + "</h1>");},1000);
+            }
+      }
+  }
+
+    $("#strict").on("click", function(){
+      if (strict == false){
+        strict = true;
+        $("#strict").toggleClass("btn-default , btn-danger");
+      }
+    })
+
+  $("#green").on("click", function(){
+     player.push(1);
+     checkMove(1);
+     tone1.play();
+  });
+  $("#red").on("click", function(){
+     player.push(2);
+     checkMove(2);
+     tone2.play();
+  });
+  $("#yellow").on("click", function(){
+     player.push(3);
+     checkMove(3);
+     tone3.play();
+  });
+  $("#blue").on("click", function(){
+     player.push(4);
+     checkMove(4);
+     tone4.play();
+  });
+
+  $("#reset").on("click" , function(){
+    if (strict == true){
+      $("#strict").toggleClass("btn-default , btn-danger");
+      strict = false;
+    }
+    getMoves();
+    startRound();
+
+  });
+
+});
